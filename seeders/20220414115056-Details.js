1c29d5c5243384a4a7305976305c9e96512e7fc1
kaito <k.s25skt@gmail.com>
2022-04-14 21:04:47 +0900

    add seeder

--- seeders/20220414115056-Details.js
+++ seeders/20220414115056-Details.js
@@ -11,6 +11,18 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await queryInterface.bulkInsert('Details', [
+      {
+        category_id: 1,
+        payment_id: 1,
+        user_id: 1,
+        cost: 7200,
+        date: '20220414',
+        description: 'イオン',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
@@ -20,5 +32,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Details', null, {});
   }
 };
