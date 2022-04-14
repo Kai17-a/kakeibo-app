d2a6495645cefc7b038af0eeecdc168edcfcb751
kaito <k.s25skt@gmail.com>
2022-04-14 20:52:35 +0900

    add Detail model

---
+++ migrations/20220414114742-create-details.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Details', {
+      detail_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      payment_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      cost: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Details');
+  }
+};
