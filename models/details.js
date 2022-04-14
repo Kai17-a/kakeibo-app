9cb2a5e0b288c0e1823e1dd19f6a6d6fc19102dc
kaito <k.s25skt@gmail.com>
2022-04-14 20:52:53 +0900

    add Models

---
+++ models/details.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Details extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Details.init({
+    detail_id: DataTypes.INTEGER,
+    category_id: DataTypes.INTEGER,
+    payment_id: DataTypes.INTEGER,
+    user_id: DateTypes.INTEGER,
+    cost: DataTypes.INTEGER,
+    date: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Details',
+  });
+  return Details;
+};
