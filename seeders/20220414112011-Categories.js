4d8d36f7cdab3f6fa19ae9d201510ed6c6e67ca9
kaito <k.s25skt@gmail.com>
2022-04-14 20:38:26 +0900

    add categories seeder

---
+++ seeders/20220414112011-Categories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Categories', [
+      {
+        category_name: '食費',
+        description: '毎月の食費',
+        user_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Categories', null, {});
+  }
+};
