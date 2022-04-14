79982955f1b97ef962ae6c2a74ae10838a8ad5f8
kaito <k.s25skt@gmail.com>
2022-04-14 20:43:59 +0900

    add seeder Payment

---
+++ seeders/20220414114017-Payments.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Payments', [
+      {
+        payment_name: 'SMBC',
+        payment_code: 'V',
+        user_id: 1,
+        description: '三井住友VISAカード',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Payments', null, {});
+  }
+};
