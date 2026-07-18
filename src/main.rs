7c79b50949b433ccbc12ff942388b791a65fe209
Kai17-a <k.s25skt@gmail.com>
2026-07-19 08:56:36 +0900

    refactor(db): DB層のモジュール分割

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod database;
-
+use crate::database::database::migrate;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    if let Err(error) = database::migrate() {
+    if let Err(error) = migrate() {
         eprintln!("Failed to initialize the database: {error}");
         return ExitCode::FAILURE;
     }
